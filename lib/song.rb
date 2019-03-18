7b6ae022de83545a2b54e8349fce840913d19392
Adam S <adam.sarli@gmail.com>
2019-03-18 15:06:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,10 @@ class Song
     self.class.all << self
   end
   
-  def self.create(name, artist)
+  def self.create
     song = self.new 
-    song.name = name 
-    song.artit = artist_name 
+  #  song.name = name 
+   # song.artit = artist_name 
     song.save 
     song 
   end 
