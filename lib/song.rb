d3eda610cf59d67f40847211d274c66b0dc10cfd
Adam S <adam.sarli@gmail.com>
2019-03-18 15:17:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -32,6 +34,7 @@ class Song
 
   def self.find_by_name(name)
     self.all.find do |song|
+      include.pry 
       song.name == song 
         
     end 
