c7a1ed91edefa4b97ca08b7baf5bf71d49ee8dfb
Adam S <adam.sarli@gmail.com>
2019-03-18 15:31:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,13 +34,13 @@ class Song
   
   def self.new_from_filename(filename)
     split_file = filename.split("-")
-    song = split_file[0]
+    song = split_file[1]
     self.new_by_name(song)
   end 
   
   def self.create_from_filename(filename)
     split_file = filename.split("-")
-    name = split_file[0]
+    name = split_file[1]
     song = self.create_by_name(name)
     song
   end 
